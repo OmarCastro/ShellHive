73d3e5e8cc491f698ecead2d26eee44a218e06f6
Omar Castro <omar.castro.360@gmail.com>
2014-04-25 00:47:06 +0100

    change to drone.io

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp        = require('gulp');
+var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     source      = require('vinyl-source-stream'),
     watchify    = require('watchify'),
     tsc         = require('gulp-tsc'),
     ls          = require('gulp-livescript'),
-    newer       = require('gulp-newer');
+    newer       = require('gulp-newer'),
     tinylr      = require('tiny-lr'),
-    runSequence = require('run-sequence');
+    istanbul    = require('gulp-istanbul')
+    runSequence = require('run-sequence'),
     mocha       = require('gulp-mocha'),
-    cover       = require('gulp-coverage');
     livereload  = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
     server      = tinylr();
 
@@ -32,24 +32,27 @@ gulp.task('ls', function() {
 
 gulp.task('mocha', function() {
   return gulp.src('test/test.js')
-    .pipe(mocha({reporter: 'spec', require:["should"]}))
+    .pipe(mocha({reporter: 'spec'}))
     .on('error', function (error) {
       var errorText = (error.plugin) ? error : error.stack;
       gutil.beep();
       gutil.log(gutil.colors.yellow(errorText));
     });
 }); 
-gulp.task('coverage', function() {
 
-gulp.src('test/test.js', { read: false })
-  .pipe(cover.instrument({
-    pattern: ['target/**/commands/[_a-g]*.js','target/parser/parser.js'],
-    debugDirectory: 'debug'
-  }))
-  .pipe(mocha({reporter: 'spec', require:["should"]}))
-  .pipe(cover.report({
-    outFile: 'coverage.html'
-  }));
+gulp.task('coverage', function() {
+  gulp.src(['target/**/commands/*.js','target/parser/parser.js'])
+    .pipe(istanbul()) // Covering files
+    .on('end', function () {
+      gulp.src(['test/test.js'])
+        .pipe(mocha({reporter: 'spec'}))
+        .on('error', function (error) {
+          var errorText = (error.plugin) ? error : error.stack;
+          gutil.beep();
+          gutil.log(gutil.colors.yellow(errorText));
+        })
+        .pipe(istanbul.writeReports()); // Creating the reports after tests runned
+    });
 });  
 
 gulp.task('watchify', function () {
@@ -73,8 +76,8 @@ gulp.task('watchify', function () {
 
 
 gulp.task('watch', function () {
-   gulp.watch('src/**/*.ts',function(event) { runSequence('ts','mocha') });
-   gulp.watch('src/**/*.ls',function(event) { runSequence('ls','mocha') });
+   gulp.watch('src/**/*.ts',function(event) { runSequence('ts','coverage') });
+   gulp.watch('src/**/*.ls',function(event) { runSequence('ls','coverage') });
    gulp.watch('test/**/*.js',['mocha']);
 });
 
