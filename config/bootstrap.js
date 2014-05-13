3408c8a6c92a5734a05e070100702c9a5d7ff83e
Omar Castro <omar.castro.360@gmail.com>
2014-05-13 01:30:06 +0100

    improve **realism** a bit on the dummy data of the bootstrap, improve UI of the server, quick fix of an issue found

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -7,53 +7,6 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#documentation
  */
-/*
-module.exports.bootstrap = function (cb) {
-/*  User.count().exec(function(err, count) {
-    if(err) {
-      sails.log.error('Already have data.');
-      return cb(err);
-    }
-    if(count > 0) return cb();
-
-		User.create(dummyUsers).exec(cb);
-  });*
-  var dummyUsers = [{
-    name: "Omar Castro",
-    email:"ei08158@fe.up.pt",
-    password: "ei08158"
-  }, {
-      name: "Omar Castillo",
-      email:"omar.castro@fe.up.pt",
-      password: "ei08158"  
-  }]
-  User.create(dummyUsers).exec(function(err,res){
-    sails.log(err,res);
-    Project.create({name:"mimi", members:res}).exec(function(err,res){
-     res.members.add(1)
-     res.members.add(2)
-     res.save(sails.log);
-    })
-    Graph.create([{name: "mimi", project:1, isRoot:true},{name:"momo", project:1}]).exec(sails.log)
-      //User.findOne(1).populate('projects').exec(function(err,res){sails.log(res.toJSON())})
-
-    Project.findOne(1).populate('members').populate('graphs').exec(function(err,res){sails.log(res.toJSON())})
-    GraphGeneratorService.addToGraph(1,"cat json.txt | grep mimi");
-    GraphGeneratorService.addToGraph(2,"ls | grep c");
-    //Graph
-    //.findOne(1)
-    //.populate("components")
-    //.populate("connections")
-    //.exec(function(err,res){sails.log(res.toJSON())})
-    GraphGeneratorService.compileGraph(1,sails.log);
-
-  })
- 
-  // It's very important to trigger this callack method when you are finished 
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
-};// */
-
 
 module.exports.bootstrap = function (cb) {
   // It's very important to trigger this callack method when you are finished 
@@ -68,7 +21,7 @@ module.exports.bootstrap = function (cb) {
   function createDummyUserData(done) {
     var dummyUsers = [
     {
-      name: "Super Administrator",
+      name: "Administrador el Gran Sheriff",
       email:"admin@admin.pt",
       password: "admin123"
     },
@@ -88,7 +41,7 @@ module.exports.bootstrap = function (cb) {
   function createDummyProjects(done) {
     var dummyProjects = [
     {
-      name:"my first project",
+      name:"El miel picante",
       members: [1,2]
     },
     ];
